63d7b43f99a0c725e06388f32a5b9d617c429abf
worry <3831444914@qq.com>
2025-05-14 16:08:22 +0800

    创建了一些类

---
+++ AI.h
@@ -0,0 +1,15 @@
+#pragma once
+#include"Chess.h"
+class AI
+{
+public:
+	void init(Chess* chess);
+	void go();
+private:
+	Chess* chess;
+	// 存储各个点位的评分情况，作为AI下棋依据
+	vector<vector<int>> scoreMap;
+	void calculateScore();
+	ChessPos think();  //private权限
+};
+
